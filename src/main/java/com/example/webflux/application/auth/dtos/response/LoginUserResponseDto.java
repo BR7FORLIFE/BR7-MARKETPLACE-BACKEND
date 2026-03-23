8312401a429d4800f293899c0b76aaf3a1581fa2
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-03-23 07:10:30 -0500

    fix: remove some files

--- src/main/java/com/example/webflux/application/auth/dtos/response/LoginUserResponseDto.java
+++
@@ -1,5 +0,0 @@
-package com.example.webflux.application.auth.dtos.response;
-
-public record LoginUserResponseDto(String access_token) {
-    
-}
--- src/main/java/com/example/webflux/domain/Authorization/ports/rols/RolPermissionRepositoryPort.java
+++
@@ -1,5 +0,0 @@
-package com.example.webflux.domain.Authorization.ports.rols;
-
-public interface RolPermissionRepositoryPort {
-    
-}
