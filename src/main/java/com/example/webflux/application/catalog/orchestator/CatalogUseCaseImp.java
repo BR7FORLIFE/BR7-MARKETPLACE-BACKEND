f624866cf3dd9461ab827be76d7833346e0ae034
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-04-14 16:11:09 -0500

    feat: add service annotation to CatalogUseCaseImp class

--- src/main/java/com/example/webflux/application/catalog/orchestator/CatalogUseCaseImp.java
+++ src/main/java/com/example/webflux/application/catalog/orchestator/CatalogUseCaseImp.java
@@ -1,13 +1,16 @@
 package com.example.webflux.application.catalog.orchestator;
 
+import org.springframework.stereotype.Service;
+
 import com.example.webflux.application.catalog.command.CreateCatalogCommand;
 import com.example.webflux.application.catalog.command.CreateCatalogCommandResult;
 import com.example.webflux.application.catalog.usecase.CatalogUseCase;
 
 import reactor.core.publisher.Mono;
 
+@Service
 public class CatalogUseCaseImp implements CatalogUseCase {
-    
+
     @Override
     public Mono<CreateCatalogCommandResult> createCatalog(CreateCatalogCommand cmd) {
         return null;
