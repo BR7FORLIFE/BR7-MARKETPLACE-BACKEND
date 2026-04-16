1d34afbef58f8881d1d894cf138e468c695134c8
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-04-15 21:12:22 -0500

    feat: implement createCatalog method in CatalogController to handle catalog creation

--- src/main/java/com/example/webflux/infrastructure/catalog/controllers/CatalogController.java
+++ src/main/java/com/example/webflux/infrastructure/catalog/controllers/CatalogController.java
@@ -1,16 +1,21 @@
 package com.example.webflux.infrastructure.catalog.controllers;
 
-import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties.Authentication;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.webflux.application.catalog.command.CreateCatalogCommand;
 import com.example.webflux.application.catalog.dto.request.CreateCatalogRequestDto;
 import com.example.webflux.application.catalog.dto.response.CreateCatalogResponseDto;
 import com.example.webflux.application.catalog.usecase.CatalogUseCase;
+import com.example.webflux.infrastructure.security.CustomUserDetails;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -26,7 +31,15 @@ public class CatalogController {
     @PostMapping
     public Mono<ResponseEntity<CreateCatalogResponseDto>> createCatalog(@RequestBody CreateCatalogRequestDto dto,
             Authentication authentication) {
-        return null;
+        CustomUserDetails details = (CustomUserDetails) authentication.getPrincipal();
+        UUID userId = details.getUserId();
+
+        CreateCatalogCommand cmd = new CreateCatalogCommand(dto.slug(), dto.nameCatalog(), dto.typeCatalog(),
+                dto.status(), dto.visible(), userId);
+
+        return catalogUseCase.createCatalog(cmd)
+                .map(res -> ResponseEntity.status(HttpStatus.CREATED).body(new CreateCatalogResponseDto(res.code(),
+                        res.typeCatalog(), res.status(), res.visible(), res.message())));
     }
 
 }
